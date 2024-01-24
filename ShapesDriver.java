4090479624652284c40633538ba578d80a80bf99
Vincenzo Sollena <99370277+Programmare123@users.noreply.github.com>
2024-01-23 21:30:40 -0500

    Add files via upload

---
+++ ShapesDriver.java
@@ -0,0 +1,12 @@
+package classlab.week2;
+
+public class ShapesDriver {
+	public static void main(String[]args) {
+		Rectangle rl = new Rectangle("small rectangle", 2, 4);
+		Square sl = new Square("big square", 200);
+		
+		System.out.println(rl);
+		System.out.print(sl);
+	}
+
+}
---
+++ StudentDriver.java
@@ -0,0 +1,10 @@
+package classlab.week3;
+
+public class StudentDriver {
+	public static void main(String[] args) {
+		Student s = new Student("you", 21, "NYC", "CS");
+		s.printAge();
+		s.printName();
+	}
+
+}
